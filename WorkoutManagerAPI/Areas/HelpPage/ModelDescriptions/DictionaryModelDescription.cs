0cba29889b764d2cc5cc5f27caabc3a898eed961
mEllingson <mitch.w.ellingson@gmail.com>
2018-08-09 13:40:10 -0500

    Removed Areas and help page

--- WorkoutManagerAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WorkoutManagerAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WorkoutManagerAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WorkoutManagerAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
